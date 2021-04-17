ffc47c465c87ec7ed8324f339d64f9155f9f14a2
zs <zs@wc.com>
2021-04-17 09:43:22 +0800

    update2

--- src/main/java/com/wc/domain/Person.java
+++ src/main/java/com/wc/domain/Person.java
@@ -4,21 +4,6 @@ public class Person {
     private String name;
     private int age;
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public static void main(String[] args) {
         System.out.println("test");
